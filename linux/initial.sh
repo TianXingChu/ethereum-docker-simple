6f3a5fb8d83d8a30508d410f4c7cc1a828d53c87
储天行 <chutianxing@kuark.com>
2017-11-23 18:41:37 +0800

    修正linux shell行连接符.

--- linux/initial.sh
+++ linux/initial.sh
@@ -1,20 +1,20 @@
 docker volume remove ethereum
 docker volume create ethereum
-docker run^
- -it^
- --rm^
- -v ethereum:/data^
- chutianxing/ethereum-docker-simple account new^
- --datadir "/data/ethereum"^
- --password /opt/password^
+docker run \
+ -it \
+ --rm \
+ -v ethereum:/data \
+ chutianxing/ethereum-docker-simple account new \
+ --datadir "/data/ethereum" \
+ --password /opt/password \
  /opt/genesis.json
 
-docker run^
- -it^
- --rm^
- -v ethereum:/data^
- chutianxing/ethereum-docker-simple^
- --datadir "/data/ethereum"^
- --ethash.dagdir "/data/ethash"^
- init^
+docker run \
+ -it \
+ --rm \
+ -v ethereum:/data \
+ chutianxing/ethereum-docker-simple \
+ --datadir "/data/ethereum" \
+ --ethash.dagdir "/data/ethash" \
+ init \
  /opt/genesis.json
